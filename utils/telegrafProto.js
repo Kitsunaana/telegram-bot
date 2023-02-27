579006004b0befcaac7f098d34d6a421eed49ccf
Selimkhanov Deni <selimkhanov_deni@list.ru>
2023-02-27 18:32:20 +0700

    27 февраля 2023

--- utils/telegrafProto.js
+++ utils/telegrafProto.js
@@ -14,7 +14,8 @@ const initProto = (bot) => {
     bot.__proto__.newCommand = (...args) => {
         commands.push(isObj(args[0]) ? args[0] : { command: args[0], description: 'empty command' })
         let newArgs = args.map((item, index) => index === 0 && isObj(item) ? item.command : item)
-        if (typeof args[1] === 'boolean' && args[1]) { newArgs = newArgs.map(checkMiddleware(args[1])).filter(item => item !== null) }
+        if (typeof args[1] === 'boolean' && args[1]) 
+            { newArgs = newArgs.map(checkMiddleware(args[1])).filter(item => item !== null) }
     
         bot.command(...newArgs)
     }
